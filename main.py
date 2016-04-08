bd851e1a62ad3fb40ab292aa1fb2bac353a36059
klaus993 <lungwitz.klaus@gmail.com>
2016-04-08 15:22:23 -0300

    Board initialization complete

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def print_column_letters():
     '''Prints the column identificatory letters (A,B..) 
     in the upper side of the board
     '''
-    for letter in RANGE[:BOARDSIZE]:
+    for letter in ascii_uppercase[:BOARDSIZE]:
         print(letter,end=' ')
     print()
 
