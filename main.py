c8e3b35ef75e069afcf45b3c3c3c7ba2e3805dae
klaus993 <lungwitz.klaus@gmail.com>
2016-04-08 15:16:10 -0300

    Board initialization complete

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 from string import ascii_uppercase
 
-BOARDSIZE=10
+BOARDSIZE = 10
 
-def initialize_board():
+def empty_board():
     ''' Initializes the board, as an empty board.
     Returns the board (list)
     '''
-    emptylist=['']
-    board=[]
+    emptylist = ['']
+    board = []
     for i in range(BOARDSIZE):
         board.append(emptylist*BOARDSIZE)
     return board
+
+def initialize_board():
+    board = empty_board()
+    if BOARDSIZE%2!=0:
+        return None
+    centerindex2 = BOARDSIZE//2
+    centerindex1 = centerindex2 - 1
+    board[centerindex1][centerindex1] = 'X'
+    board[centerindex1][centerindex2] = 'O'
+    board[centerindex2][centerindex1] = 'O'
+    board[centerindex2][centerindex2] = 'X'
+    return board
+
 
 def print_column_letters():
     '''Prints the column identificatory letters (A,B..) 
     in the upper side of the board
     '''
-    RANGE='1234567890'
+    RANGE = '1234567890'
     for letter in RANGE[:BOARDSIZE]:   #THIS COMMIT IS FOR TESTS 2
         print(letter,end=' ')
     print()
@@ -27,12 +40,12 @@ def print_board(board):
     print(' '*4,end='')                         # Spaces printed for column letter fitting
     print_column_letters()
     for index,row in enumerate(board):
-        if index+1<10:                         # Validates if row number is 10 
+        if index+1 < 10:                         # Validates if row number is 10 
             print(' '+str(index+1),end=' ')    # or more for space fitting
         else:
             print(index+1,end=' ')
         for column in row:
-            if column=='':
+            if column == '':
                 print('|'+' ',end='')
             else:
                 print('|'+column,end='')
