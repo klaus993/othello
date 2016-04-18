9a2e4ecb96be67ac2d62d38cda6fb04f3ff373f3
klaus993 <lungwitz.klaus@gmail.com>
2016-04-18 15:11:00 -0300

    comments added

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def print_col_letters():
     print(' '*4, end='')    # Spaces printed for column letter fitting
     for i in range(len(create_and_initialize_board())):
         print(chr(i+65), end=' ')  # Adds 65 to match ASCII uppercase code
-    print()
+    print()                        # Ends with a new line for board print
 
 
 def print_board(board):
@@ -60,7 +60,7 @@ def print_board(board):
     print_col_letters()
     for index, row in enumerate(board):
         if (index+1) < 10:                      # Validates if row number is 10
-            print(' '+str(index+1), end=' ')  # or more for space fitting
+            print(' '+str(index+1), end=' ')    # or more for space fitting
         else:
             print(index+1, end=' ')
         for col in row:
@@ -101,22 +101,24 @@ def enter_chip(board, player):
     """
     chip_location = ask_input(player)
     if not chip_location or chip_location[0] not in ascii_letters or chip_location[1] != ' ' or not chip_location[2].isdigit():
+    # Covers every other input than the correct syntax (column row)
         return False
     col = return_col(chip_location)
     row = int(return_row(chip_location))
     col_ascii = ord(col)
-    literal_col = col_ascii-65
-    literal_row = row-1
-    if (not True in valid_directions(literal_row, literal_col, board, player)):
-        return False
+    literal_col = col_ascii-65     # Column number corresponding to list index.
+    literal_row = row-1            # Row number corresponding to list index.
     flag = False
     for i in range(8):
         if valid_directions(literal_row, literal_col, board, player)[i]:
+            # Checks directions in which to convert enemy chips
             board = chip_turn(literal_row, literal_col, incrementers[i][0], incrementers[i][1], board, player)
             flag = True
     if flag:
         board[literal_row][literal_col] = PLAYER_CHIPS[player]
-    return board
+        return board
+    else:
+        return False
 
 
 def is_valid_direction(old_row, old_col, row_add, col_add, board, player):
@@ -125,17 +127,17 @@ def is_valid_direction(old_row, old_col, row_add, col_add, board, player):
     """
     current_row = old_row + row_add
     current_col = old_col + col_add
-    if (current_row >= BOARD_SIZE or current_col >= BOARD_SIZE or
-        (board[current_row][current_col] == PLAYER_CHIPS[player] and
+    if (current_row >= BOARD_SIZE or current_col >= BOARD_SIZE or      # Checks all the posibilities
+        (board[current_row][current_col] == PLAYER_CHIPS[player] and   # which result in an invalid conversion
             board[old_row][old_col] == PLAYER_CHIPS[player]) or
         (board[old_row][old_col] == PLAYER_CHIPS[player] or
             (not board[current_row][current_col]) or
             (not board[old_row][old_col] and
                 board[current_row][current_col] == PLAYER_CHIPS[player]))):
         return False
-    if (board[old_row][old_col] != PLAYER_CHIPS[player] and
-            board[current_row][current_col] == PLAYER_CHIPS[player]):
-        return True
+    if (board[old_row][old_col] != PLAYER_CHIPS[player] and            # Checks whether previous chip is 
+            board[current_row][current_col] == PLAYER_CHIPS[player]):  # from the enemy player, and current 
+        return True                                                    # if is of the current player
     return is_valid_direction(current_row, current_col, row_add, col_add, board, player)
 
 
@@ -188,7 +190,7 @@ def check_all_moves(board, player):
     return True in moves
 
 
-def winner(board):
+def declare_winner(board):
     """Given a board, checks the winner of the game by counting
     the chips of both players. Prints a message and the correspondings points
     """
@@ -210,8 +212,9 @@ def main():
     """
     print('¡Bienvenido al Reversi!')
     sleep(1)
-    turn_count = 1
-    playing = True
+    turn_count = 1         # Turn counter, used for 60 turns limit.
+    no_plays_count = 0     # Used in case there is no more possible moves for 
+    playing = True         # any player, if it reaches 2, breaks the cycle and ends the game.
     board = create_and_initialize_board()
     if check_boardsize():
         while playing:
@@ -219,16 +222,20 @@ def main():
             for i in range(2):
                 print_board(board)
                 if check_all_moves(board, i):
-                    while not enter_chip(board, i):
+                    while not enter_chip(board, i):  # Enters the cycle only if there is an input error.
                         print('Sintaxis incorrecta o movimiento inválido. Vuelva a ingresar la ficha.')
+                    if no_plays_count != 0:         # Resets counter in case there was
+                        no_plays_count = 0          # only one play with no possible moves.
                 else:
                     print('Color '+PLAYER_COLORS[i]+': no tienes jugadas posibles')
+                    no_plays_count += 1
                     sleep(1)
-                    break
-            turn_count += 1
-            if turn_count == 4:
+            if no_plays_count == 2:      # If it reaches 2 it means that two consecutive
+                break                    # plays were not possible, in that case the cycle
+            turn_count += 1              # breaks and the game ends.
+            if turn_count == 61:
                 playing = False
-        winner(board)
+        declare_winner(board)
     else:
         print('El tamaño del tablero debe ser par')
 
