d9ace1747ff2a3936630768e6151ebc6c0fb0729
klaus993 <lungwitz.klaus@gmail.com>
2016-04-19 20:45:39 -0300

    Fixed input error in enter_chip function, refactored declare_winner function

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from time import sleep
-from string import ascii_letters
+# from string import ascii_letters
 
 
 # BOARD_SIZE must be between [4, 26] and even
-BOARD_SIZE = 8
+BOARD_SIZE = 2
 PLAYERS = 0, 1
 PLAYER_0 = '\033[01mB\033[0m'           # White bold 'B'
 PLAYER_1 = '\033[01m\033[90mN\033[0m'   # Dark grey bold 'N'
@@ -27,7 +27,7 @@ def create_board():
     empty_list = ['']
     board = []
     for i in range(BOARD_SIZE):
-        board.append(empty_list*BOARD_SIZE)
+        board.append(empty_list * BOARD_SIZE)
     return board
 
 
@@ -36,11 +36,11 @@ def create_and_initialize_board():
     takes the empty board and adds starting chips in the center
     """
     board = create_board()
-    center_index_1 = BOARD_SIZE//2
+    center_index_1 = BOARD_SIZE // 2
     center_index_2 = center_index_1 - 1
     board[center_index_2][center_index_2] = PLAYER_CHIPS[0]
     board[center_index_2][center_index_1] = PLAYER_CHIPS[1]
-    board[center_index_1][center_index_2] = PLAYER_CHIPS[1]
+    board[center_index_1][center_index_2] = PLAYER_CHIPS[0]
     board[center_index_1][center_index_1] = PLAYER_CHIPS[0]
     return board
 
@@ -49,9 +49,9 @@ def print_col_letters():
     """ Prints the column identification letters
     (A,B,C,..) in the upper side of the board
     """
-    print(' '*4, end='')    # Spaces printed for column letter fitting
+    print(' ' * 4, end='')    # Spaces printed for column letter fitting
     for i in range(len(create_and_initialize_board())):
-        print(chr(i+65), end=' ')  # Adds 65 to match ASCII uppercase code
+        print(chr(i + 65), end=' ')  # Adds 65 to match ASCII uppercase code
     print()                        # Ends with a new line for board print
 
 
@@ -61,15 +61,15 @@ def print_board(board):
     """
     print_col_letters()
     for index, row in enumerate(board):
-        if (index+1) < 10:                      # Validates if row number is 10
-            print(' '+str(index+1), end=' ')    # or more for space fitting
+        if (index + 1) < 10:                      # Validates if row number is 10
+            print(' ' + str(index + 1), end=' ')    # or more for space fitting
         else:
-            print(index+1, end=' ')
+            print(index + 1, end=' ')
         for col in row:
             if col == '':
-                print('|'+' ', end='')
+                print('|' + ' ', end='')
             else:
-                print('|'+col, end='')
+                print('|' + col, end='')
         print('|')
 
 
@@ -77,7 +77,7 @@ def ask_input(player):
     """ Asks the user the position to enter a chip (col,row)
     and returns the chip location as a string (i.e. A 5)
     """
-    chip_location = input('Color '+PLAYER_COLORS[player]+': '+INPUT_PROMPT)
+    chip_location = input('Color ' + PLAYER_COLORS[player] + ': ' + INPUT_PROMPT)
     return chip_location
 
 
@@ -102,15 +102,15 @@ def enter_chip(board, player):
     Also checks syntax for user chip input and move validity.
     """
     chip_location = ask_input(player)
-    if not chip_location or chip_location[0] not in ascii_letters or chip_location[1] != ' ' or not chip_location[2].isdigit():
+    if len(chip_location) < 3 or not chip_location or not chip_location[0].isalpha() or not chip_location[1].isspace() or chip_location.isspace() or not chip_location[2:].isdigit():
             # Covers every other input than the correct syntax (column row)
         return False
     col = return_col(chip_location)
     row = int(return_row(chip_location))
     col_ascii = ord(col)
-    literal_col = col_ascii-65     # Column number corresponding to list index.
-    literal_row = row-1            # Row number corresponding to list index.
-    if literal_row not in range(BOARD_SIZE) or literal_col not in range (BOARD_SIZE) or board[literal_row][literal_col] != '':
+    literal_col = col_ascii - 65     # Column number corresponding to list index.
+    literal_row = row - 1            # Row number corresponding to list index.
+    if literal_row not in range(BOARD_SIZE) or literal_col not in range(BOARD_SIZE) or board[literal_row][literal_col] != '':
         return False
     flag = False
     for i in range(8):
@@ -198,17 +198,15 @@ def declare_winner(board):
     """Given a board, checks the winner of the game by counting
     the chips of both players. Prints a message and the correspondings points
     """
-    player_0_points = count_chips(board, 0)
-    player_1_points = count_chips(board, 1)
-    if player_0_points > player_1_points:
-        print('¡El color '+PLAYER_COLORS[0]+' ha ganado la partida!')
-    elif player_0_points == player_1_points:
+    winner = '¡El color {} ha ganado la partida!'
+    for i in range(2):
+        if count_chips(board, i) > count_chips(board, i - 1):
+            print(winner.format(PLAYER_COLORS[i]))
+    if player_0_points == player_1_points:
         print('¡Empate!\n')
-    else:
-        print('¡El color '+PLAYER_COLORS[1]+' ha ganado la partida!')
     print('Puntajes:')
-    print(PLAYER_COLORS[0].title()+': '+str(player_0_points)+' punto(s)')
-    print(PLAYER_COLORS[1].title()+': '+str(player_1_points)+' punto(s)')
+    for i in range(2):
+        print('{}: {} punto(s)'.format(PLAYER_COLORS[i].title(), count_chips(board, i)))
 
 
 def main():
@@ -221,19 +219,19 @@ def main():
     board = create_and_initialize_board()
     if check_boardsize():
         while playing:
-            print('Turno '+str(turn_count))
+            print('Turno ' + str(turn_count))
             for i in range(2):
                 print_board(board)
                 if check_all_moves(board, PLAYERS[i]):
                     while not enter_chip(board, i):  # Enters the cycle only if there is an input error.
                         print('Sintaxis incorrecta o movimiento inválido. Vuelva a ingresar la ficha.')
                 else:
-                    if not check_all_moves(board, PLAYERS[i-1]):
+                    if not check_all_moves(board, PLAYERS[i - 1]):
                         print('Ningun color tiene jugadas posibles.')
                         playing = False
                         break
                     sleep(1)
-                    print('Color '+PLAYER_COLORS[i]+': no tienes jugadas posibles')
+                    print('Color ' + PLAYER_COLORS[i] + ': no tienes jugadas posibles')
             turn_count += 1              # breaks and the game ends.
             if turn_count == MAX_TURNS:
                 playing = False
